4bcc09ad6d243be54a1ba9647124e711bedbb612
jimxshaw <jimxshaw@gmail.com>
2025-04-06 09:54:23 -0500

    Start implementation of Driver class.

--- src/P2Driver.java
+++ src/P2Driver.java
@@ -1,13 +1,41 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.*;
+
 public class P2Driver {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        if (args.length != 2) {
+            System.out.print("Error Incorrect Arguments:" + Arrays.toString(args));
+            System.exit(0);
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            File inputFile = new File(args[0]);
+            File outputFile = new File(args[1]);
+
+            Scanner scanner = new Scanner(inputFile);
+            PrintWriter writer = new PrintWriter(outputFile);
+
+            LazyAVLTree tree = new LazyAVLTree();
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim();
+
+                // Find out the command.
+                // E.g. Insert, Delete, Contains, etc.
+                String command = line.contains(":") ? line.substring(0, line.indexOf(":")) : line;
+
+                switch (command) {
+
+                    default:
+                        writer.println("Error with line: " + line);
+                        break;
+                }
+
+            }
+        }
+        catch (Exception e) {
+            System.err.println("Error occurred while processing the files: " + e.getMessage());
         }
     }
 }
